5c3f47fd8fad64e3e3836920413d476680323f5a
Artemy Medvedev <artemiy.logmy@gmail.com>
2025-03-26 17:04:06 +0500

    kaggle competition .csv file generator, little code adjustements for the digit recognition model

--- digitsrec.py
+++ digitsrec.py
@@ -1,14 +1,25 @@
 import torch
+import csv
 
 # reading the data
 f = open("digits.txt")
 content = f.readlines()
 f.close()
 
+f = open("digits-test.txt")
+content_test = f.readlines()
+f.close()
+
 images = []
 ans = []
 images_test = []
 ans_test = []
+
+full_dataset = []
+# creating full dataset
+for img in content_test[:len(content_test)]:
+    imgspl = img.split(",")
+    full_dataset.append([1 if int(x)> 10 else 0 for x in imgspl])
 
 # creating train dataset
 for img in content[:int(0.8*len(content))]:
@@ -28,6 +39,9 @@ ans = torch.tensor(ans)
 images_test = torch.tensor(images_test).float()
 ans_test = torch.tensor(ans_test)
 
+full_dataset = torch.tensor(full_dataset).float()
+
+# initializing the network's weights and biases
 g = torch.Generator().manual_seed(2147483647)
 
 w = torch.randn((784, 10), generator=g)
@@ -37,9 +51,9 @@ params = [w, b]
 for p in params:
     p.requires_grad = True
 
-# training dataset
-miniBatchCount = 300
-for i in range(300000):
+# training the network
+miniBatchCount = 100
+for i in range(200000):
     minibatch = torch.randint(0, images.shape[0], (miniBatchCount,), generator=g)
 
     logits = torch.tanh(images[minibatch] @ w+b)
@@ -58,8 +72,10 @@ for i in range(300000):
 
     loss.backward()
 
-    learningAlpha = 0.1
+    learningAlpha = 0.2
 
+    if loss.data < 2:
+        learningAlpha = 0.1
     if loss.data < 1.3:
         learningAlpha = 0.07
     if loss.data < 1.15:
@@ -106,7 +122,7 @@ loss = -prob[torch.arange(prob.shape[0]), ans_test].log().mean()
 print("Test loss:")
 print(loss)
 
-
+# predicting the external image digit
 cur_img = torch.tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]).float()
 logits = torch.tanh(cur_img @ w+b)
 
@@ -116,3 +132,26 @@ prob = counts/counts.sum(1, keepdim=True)
 
 guess = list(list(prob)[0]).index(max(list(list(prob)[0])))
 print("External image guess: ", guess)
+
+
+
+
+# writing the whole dataset predictions in a .csv file
+guesses = [
+    ["ImageId", "Label"]
+]
+for i in range(len(full_dataset)):
+    cur_img = full_dataset[[i]]
+    logits = torch.tanh(cur_img @ w+b)
+
+    counts = logits.exp()
+
+    prob = counts/counts.sum(1, keepdim=True)
+
+    guess = list(list(prob)[0]).index(max(list(list(prob)[0])))
+
+    guesses.append([i+1, guess])
+
+with open("output.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(guesses)
