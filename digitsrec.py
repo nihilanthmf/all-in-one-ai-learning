6a29e66730c49448340961107782e6fae7c78d08
Artemy Medvedev <artemiy.logmy@gmail.com>
2025-03-26 12:33:48 +0500

    external image upload function + little improvements to the image rec model

--- digitsrec.py
+++ digitsrec.py
@@ -38,8 +38,8 @@ for p in params:
     p.requires_grad = True
 
 # training dataset
-miniBatchCount = 200
-for i in range(100000):
+miniBatchCount = 300
+for i in range(300000):
     minibatch = torch.randint(0, images.shape[0], (miniBatchCount,), generator=g)
 
     logits = torch.tanh(images[minibatch] @ w+b)
@@ -50,21 +50,41 @@ for i in range(100000):
 
     loss = -prob[torch.arange(prob.shape[0]), ans[minibatch]].log().mean()
 
-    if i % 100 == 0:
-        print(loss)
+    if i % 5000 == 0:
+        print(loss.data)
 
     for p in params:
         p.grad = None
 
     loss.backward()
 
-    learningAlpha = 0.15 if i < 1000 else 0.04
+    learningAlpha = 0.1
+
+    if loss.data < 1.3:
+        learningAlpha = 0.07
+    if loss.data < 1.15:
+        learningAlpha = 0.05
+    if loss.data < 1.11:
+        learningAlpha = 0.005
+
+    if loss.data < 1:
+        break
+
     for p in params:
         p.data -= learningAlpha * p.grad
+print(loss.data)
 
 # sampling
 print("Guesses:")
-for i in range(len(images_test)):
+for i in range(int(0.01*len(images_test))):
+    # printing the ASCII image
+    # for row in range(28):
+    #     row_str = ""
+    #     for c in range(28):
+    #         # row_str += str(list(images_test[(28*row)+c].int().data))
+    #         row_str += str( images_test.int().tolist()[(28*row)+c][0] )
+    #     print(row_str)
+
     cur_img = images_test[[i]]
     logits = torch.tanh(cur_img @ w+b)
 
@@ -85,3 +105,14 @@ prob = counts/counts.sum(1, keepdim=True)
 loss = -prob[torch.arange(prob.shape[0]), ans_test].log().mean()
 print("Test loss:")
 print(loss)
+
+
+cur_img = torch.tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]).float()
+logits = torch.tanh(cur_img @ w+b)
+
+counts = logits.exp()
+
+prob = counts/counts.sum(1, keepdim=True)
+
+guess = list(list(prob)[0]).index(max(list(list(prob)[0])))
+print("External image guess: ", guess)
---
+++ readimage.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+im = Image.open('two.jpg') # Can be many different formats.
+pix_val = list(im.getdata())
+print(pix_val)
+res = []
+for i in pix_val:
+    if i > (200,200,200,200):
+        res.append(0)
+    else:
+        res.append(1)
+print(res)
+
+# printing the image in ascii
+for row in range(28):
+    row_str = ""
+    for c in range(28):
+        row_str += str(res[(28*row)+c])
+
+    print(row_str)
