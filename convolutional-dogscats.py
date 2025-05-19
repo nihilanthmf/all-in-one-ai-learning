91fcb774c8f109431f3af9817d2ac29fe3c5a9fa
Artemy Medvedev <artemiy.logmy@gmail.com>
2025-05-19 23:31:03 +0500

    2 conv layers

--- convolutional-dogscats.py
+++ convolutional-dogscats.py
@@ -10,7 +10,7 @@ import numpy as np
 
 dir = "./dogs-vs-cats-redux-kernels-edition/train"
 
-device = "cuda"
+device = "cpu"
 
 # initializing the network's params
 g = torch.Generator(device=device).manual_seed(42)
@@ -21,9 +21,9 @@ convo_w_kernels = 128
 weights_scale = 0.001
 
 convo_w = torch.randn((convo_w_kernels, 3, 3, 3), generator=g, dtype=torch.float32, device=device) * weights_scale
-convo_w2 = torch.randn((convo_w_kernels, 3, 3, 3), generator=g, dtype=torch.float32, device=device) * weights_scale
+convo_w2 = torch.randn((convo_w_kernels, convo_w_kernels, 3, 3), generator=g, dtype=torch.float32, device=device) * weights_scale
 
-wh = torch.randn((4096 * convo_w_kernels, 25), generator=g, dtype=torch.float32, device=device) * weights_scale
+wh = torch.randn((1024 * convo_w_kernels, 25), generator=g, dtype=torch.float32, device=device) * weights_scale
 bh = torch.zeros((25,), device=device)
 
 wo = torch.randn((25, 10), generator=g, dtype=torch.float32, device=device) * weights_scale
@@ -67,7 +67,7 @@ def model(img, epoch):
 
     img_activations = torch.relu(img_convo)
 
-    img_pooling = f.max_pool2d(img_activations, kernel_size=2, stride=2).reshape(batch_size*2, -1)
+    img_pooling = f.max_pool2d(img_activations, kernel_size=2, stride=2)#.reshape(batch_size*2, -1)
     
     img_convo_2 = convolute(img_pooling, convo_w2)
     
@@ -75,6 +75,7 @@ def model(img, epoch):
 
     img_pooling_2 = f.max_pool2d(img_activations_2, kernel_size=2, stride=2).reshape(batch_size*2, -1)
 
+    # print(img_pooling_2.shape, img_pooling.shape)
     h = torch.tanh(img_pooling_2 @ wh + bh)
     
     logits = h @ wo + bo
